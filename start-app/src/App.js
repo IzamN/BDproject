adabccf8f7a9dbce8fe66952cb35c4a09af35750
IzamN <izam.nadya@mail.ru>
2024-10-14 21:01:27 +0300

    Update App.js
    commit

--- start-app/src/App.js
+++ start-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React love!Nadya!
+          Learn React love!
           Привет
         </a>
       </header>
